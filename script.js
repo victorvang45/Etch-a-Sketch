2fb39e4a3efff8677f2823a5cc690665e4ba15ac
victorvang45 <victorvang456@gmail.com>
2023-10-26 01:46:24 -0500

    Added 16x16 grid

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let grid = document.querySelector('board');
+let board = document.querySelector('.board');
+board.style.gridTemplateColumns = "repeat(16, 1fr)";
+board.style.gridTemplateRows = "repeat(16, 1fr)";
+
+
+for(let i = 0; i < 256; i++) {
+    let square = document.createElement('div');
+    square.style.backgroundColor = 'blue';
+    square.style.borderStyle = 'dashed';
+    board.appendChild(square);
+}

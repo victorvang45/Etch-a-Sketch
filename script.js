ba7246bf1705a861e681cbff95c702c2beb1a6c9
victorvang45 <victorvang456@gmail.com>
2023-10-26 21:44:10 -0500

    Added css styling to webpage

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let click = false;
+
+
 let board = document.querySelector('.board');
 board.style.gridTemplateColumns = "repeat(16, 1fr)";
 board.style.gridTemplateRows = "repeat(16, 1fr)";
@@ -5,7 +8,26 @@ board.style.gridTemplateRows = "repeat(16, 1fr)";
 
 for(let i = 0; i < 256; i++) {
     let square = document.createElement('div');
-    square.style.backgroundColor = 'blue';
-    square.style.borderStyle = 'dashed';
+    square.classList.add('square');
+    square.style.backgroundColor = 'white';
     board.appendChild(square);
 }
+
+let squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    
+        square.addEventListener('mouseover', () => {
+            if(click) {
+                square.style.backgroundColor = 'red';
+            }
+    });
+
+
+});
+
+document.querySelector('body').addEventListener('click', (e) => {
+    if(e.target.tagName != "BUTTON") {
+        click = !click;
+    }
+   
+})

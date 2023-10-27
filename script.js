785966d948b105fb492fbe85a4e9e4710522bfed
victorvang45 <victorvang456@gmail.com>
2023-10-26 23:22:58 -0500

    Added erasing, resizing, and reset

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
 let click = false;
+let color = 'black';
+
+function CreateBoard(size) {
+    let board = document.querySelector('.board');
+    let grid = board.querySelectorAll('div');
+    grid.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.style.borderStyle = 'groove';
+        square.style.backgroundColor = 'white';
+        board.appendChild(square);
+    }
+
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+
+        square.addEventListener('mouseover', () => {
+            if (click) {
+                square.style.backgroundColor = color;
+            }
+        });
 
 
-let board = document.querySelector('.board');
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
-
-
-for(let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.style.backgroundColor = 'white';
-    board.appendChild(square);
+    });
 }
 
-let squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    
-        square.addEventListener('mouseover', () => {
-            if(click) {
-                square.style.backgroundColor = 'red';
-            }
+
+function resetBoard() {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((square) => {
+        square.style.backgroundColor = 'white';
     });
+}
+
+function rangeFunction() {
+    let size = document.getElementById('sliderange').value;
+    document.querySelector('.sliderValue').textContent = `Grid Size: ${size} x ${size}`;
+    resetBoard();
+    CreateBoard(size);
+}
 
 
-});
+function erase() {
+    color = 'white';
+}
+
+
+CreateBoard(16);
 
 document.querySelector('body').addEventListener('click', (e) => {
-    if(e.target.tagName != "BUTTON") {
+    if (e.target.tagName != "BUTTON" && e.target.tagName != "INPUT") {
         click = !click;
+        if (click) {
+            document.querySelector('.mode').textContent = 'Mode: Sketching';
+        } else {
+            document.querySelector('.mode').textContent = 'Mode: Not Sketching';
+        }
     }
-   
+
 })
